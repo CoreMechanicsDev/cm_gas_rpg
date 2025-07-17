b43c4b4947d4bb2e14fd057065a636aa4395ada4
CoreMechanicsDev <coremechanics@icloud.com>
2025-07-16 18:00:26 -0700

    Set up spring arm camera

---
+++ Aura/Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Core Mechanics
+
+
+#include "Game/AuraGameModeBase.h"
+
--- Aura/Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Aura/Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -51,7 +51,6 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 	{
 		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
 		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
-		
 	}
 	
 }
